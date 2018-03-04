2d85b106fe035c77b6ca1e24e698b657ad4146ab
FDDA-OPS user <fdda-ops@cepri-c1-int1.rap.ucar.edu>
2018-03-04 02:55:39 +0000

    add static plots; update 2d ncl, to have const field filled & label disappear

--- ncl/non_realtime/static/run_ncl.sh
+++ ncl/non_realtime/static/run_ncl.sh
@@ -1,8 +1,17 @@
-ncl plot_hires_terrain_chan.ncl 'srcfilename="Fulldom_hires_netcdf_file.nc"' 'chanfilename="201705310000.CHRTOUT_DOMAIN1"' 'lonlat_list="113.843,113.936,22.595,22.7"' 'dom_name="TG"' 'nodeidx_file="nodeidx_TieGang.txt"'
+ncl plot_hires_terrain_chan.ncl 'srcfilename="Fulldom_hires_netcdf_file.nc"' 'chanfilename="201802070100.CHRTOUT_DOMAIN1"' 'lonlat_list="113.8289,113.9888,22.5847,22.7132"' 'dom_name="TGSK"' 'nodeidx_file="nodeidx_TGSK.txt"'
 
+ncl plot_hires_terrain_chan.ncl 'srcfilename="Fulldom_hires_netcdf_file.nc"' 'chanfilename="201802070100.CHRTOUT_DOMAIN1"' 'lonlat_list="113.9752,114.0986,22.5904,22.7372"' 'dom_name="GLHU"' 'nodeidx_file="nodeidx_GLHU.txt"'
 
-ncl plot_hires_terrain_chan.ncl 'srcfilename="Fulldom_hires_netcdf_file.nc"' 'chanfilename="201705310000.CHRTOUT_DOMAIN1"' 'lonlat_list="113.955,114.113,22.577,22.740"' 'dom_name="GL"' 'nodeidx_file="nodeidx_GuanLan.txt"'
+ncl plot_hires_terrain_chan.ncl 'srcfilename="Fulldom_hires_netcdf_file.nc"' 'chanfilename="201802070100.CHRTOUT_DOMAIN1"' 'lonlat_list="114.1710,114.3132,22.6744,22.8033"' 'dom_name="LGHU"' 'nodeidx_file="nodeidx_LGHU.txt"'
 
-ncl plot_hires_terrain_chan.ncl 'srcfilename="Fulldom_hires_netcdf_file.nc"' 'chanfilename="201705310000.CHRTOUT_DOMAIN1"' 'dom_name="D4"' 
+ncl plot_hires_terrain_chan.ncl 'srcfilename="Fulldom_hires_netcdf_file.nc"' 'chanfilename="201802070100.CHRTOUT_DOMAIN1"' 'lonlat_list="114.2565,114.3828,22.6119,22.7238"' 'dom_name="PSHU"' 'nodeidx_file="nodeidx_PSHU.txt"'
 
-ncl plot_hires_terrain_chan.ncl 'srcfilename="Fulldom_hires_netcdf_file.nc"' 'chanfilename="201705310000.CHRTOUT_DOMAIN1"' 'dom_name="SZ"'
+ncl plot_hires_terrain_chan.ncl 'srcfilename="Fulldom_hires_netcdf_file.nc"' 'chanfilename="201802070100.CHRTOUT_DOMAIN1"' 'lonlat_list="113.7877,113.9444,22.6994,22.8472"' 'dom_name="MZHU"' 'nodeidx_file="nodeidx_MZHU.txt"'
+
+ncl plot_hires_terrain_chan.ncl 'srcfilename="Fulldom_hires_netcdf_file.nc"' 'chanfilename="201802070100.CHRTOUT_DOMAIN1"' 'lonlat_list="114.0755,114.2208,22.5472,22.6607"' 'dom_name="SZSK"' 'nodeidx_file="nodeidx_SZSK.txt"'
+
+ncl plot_hires_terrain_chan.ncl 'srcfilename="Fulldom_hires_netcdf_file.nc"' 'chanfilename="201802070100.CHRTOUT_DOMAIN1"' 'lonlat_list="114.0060,114.1238,22.5021,22.5856"' 'dom_name="SZFT"' 'nodeidx_file="nodeidx_SZFT.txt"'
+
+ncl plot_hires_terrain_chan.ncl 'srcfilename="Fulldom_hires_netcdf_file.nc"' 'chanfilename="201802070100.CHRTOUT_DOMAIN1"' 'dom_name="D4"' 
+
+ncl plot_hires_terrain_chan.ncl 'srcfilename="Fulldom_hires_netcdf_file.nc"' 'chanfilename="201802070100.CHRTOUT_DOMAIN1"' 'dom_name="SZ"'
